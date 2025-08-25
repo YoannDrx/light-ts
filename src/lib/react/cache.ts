d46374f4c14795139ff8dce604451c39dc07d495
Melvyn <melvynmal@gmail.com>
2025-08-25 11:39:35 +0800

    delete useless files

--- src/lib/react/cache.ts
+++
@@ -1,7 +0,0 @@
-// To avoid calling many time same function, you can cache them with react `cache` method
-
-import { cache } from "react";
-import { getCurrentUser, getRequiredCurrentUser } from "../user/get-user";
-
-export const getCurrentUserCache = cache(getCurrentUser);
-export const getRequiredCurrentUserCache = cache(getRequiredCurrentUser);
